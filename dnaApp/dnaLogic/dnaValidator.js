dbed35576d0a8dcee6df21647e660da97b01c0ee
Rodrigo Ochoa <rodrigoa_ochoaroberts@mcafee.com>
2018-05-21 11:45:31 -0300

    First commit

---
+++ dnaApp/dnaLogic/dnaValidator.js
@@ -0,0 +1,8 @@
+// Follow http://www.samselikoff.com/blog/some-Javascript-constructor-patterns/
+var dnaValidator = function(){
+  var validator = {};
+
+  return validator;
+}
+
+module.exports = dnaValidator();

d20ae8bedd711d641106565a67b6d7c6a97da3fc
Rodrigo <rodrigo.ariel.ochoa@gmail.com>
2018-05-21 23:33:31 -0300

    First version of detection of horizontal and vertical

--- dnaApp/dnaLogic/dnaValidator.js
+++ dnaApp/dnaLogic/dnaValidator.js
@@ -1,6 +1,85 @@
+var _ = require('lodash');
 
-function DnaValidator(){
+function DnaValidator() {
   var validator = {};
+
+  function hasHorizontalMatch(word) {
+    var prevLetter = '';
+    var count = 1;
+    var matches = 0;
+    var hasMatched = false; //flag to detect matches.
+    _.each(word, function (letter) {
+      if (prevLetter === letter) {
+        count++;
+        if (count >= 4) {
+          hasMatched = true;
+        }
+      } else {
+        if (hasMatched) {
+         matches++;
+        }
+        prevLetter = letter;
+        count = 1;
+        hasMatched = false;
+      }
+    });
+    if(hasMatched) { //last match case.
+      matches++;
+    }
+    return matches; //return true if it has a horizontal match.
+  }
+
+  validator.getHorizontalMatches = function (inputValues) {
+   var matcherCounter = 0;
+    _.each(inputValues, function (word) {
+      matcherCounter = matcherCounter + hasHorizontalMatch(word);
+    });
+    return matcherCounter;
+  };
+
+  //Since the input is an array of string, we need a matrix
+  //to improve the process.
+  function sanitizeInput(inputValues) {
+    var result = [];
+    _.each(inputValues, function(row){
+      result.push(_.toArray(row)); // move into an array
+    })
+    return result;
+  }
+
+  validator.getVerticalMatches = function(inputValues) {
+    inputValues = sanitizeInput(inputValues); //TODO: remove it
+    var newValues = _.zip.apply(_, inputValues); //transpond the matrix using lodash
+    return validator.getHorizontalMatches(newValues);
+  };
+
+  function getDiagWords(i,j, inputValues)
+  {
+    if(inputValues.length - i < 4 && inputValues.length - j < 4)
+    {
+      return [];
+    }
+    var wordLeftToRigth = [];
+    //It supose that the matrix is a square
+    while(i < inputValues.length && j < inputValues.length){
+      wordLeftToRigth.push(inputValues[i][j]);
+      i++;
+      j++;
+    }
+    return wordLeftToRigth;
+  }
+
+  validator.getDiagonalMatches = function(inputValues) {
+    inputValues = sanitizeInput(inputValues); //TODO: remove it
+    var newValues = [];
+    for(var i=0; i < inputValues.length - 3; i++) //last 3 items dosen't fill a row of 4
+    {
+      for(var j=0; j <inputValues.length; j++) {
+        newValues.push(getDiagWords(i,j, inputValues));
+      }
+    }
+    return validator.getHorizontalMatches(newValues);
+  };
 
   return validator;
 }
--- dnaApp/test/dnaValidator.spec.js
+++ dnaApp/test/dnaValidator.spec.js
@@ -1,8 +1,51 @@
 var expect = require('chai').expect;
-var dnaValidator = require('../dnaLogic/dnaValidator')
+var dnaValidator = require('../dnaLogic/dnaValidator');
+
 describe('Validate dna values', function() {
     it('Returns a  valid validator object', function() {
         var validator = dnaValidator();
         expect(validator).not.to.be.null;
-    })
+    });
+
+    it('has to returns the number of horizontal matches', function(){
+        var inputSample = ['ATGCGA','CAGTGC','TTATGT','AGAAGG','CCCCTA','TCACTG'];
+        var validator = dnaValidator();
+        var result = validator.getHorizontalMatches(inputSample);
+        expect(result).to.equal(1);
+    });
+
+    it('has to detect 2 matches in the same row', function(){
+      var inputSample = ['CCCCATTTTTCGGGACCCC'];
+      var validator = dnaValidator();
+      var result = validator.getHorizontalMatches(inputSample);
+      expect(result).to.equal(3);
+    });
+
+    it('has to detect 3 matches in different rows', function(){
+      var inputSample = ['AGGGGA','CAGTGC','TTATGT','AGAAAA','CCCCTA','TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getHorizontalMatches(inputSample);
+      expect(result).to.equal(3);
+    });
+
+    it('has to detect 2 matches with same letter', function(){
+      var inputSample = ['AAAAAATAAAA'];
+      var validator = dnaValidator();
+      var result = validator.getHorizontalMatches(inputSample);
+      expect(result).to.equal(2);
+    });
+
+    it('has to detect 1 vertical match', function(){
+      var inputSample = ['ATGCGA','CAGTGC','TTATGT','AGAAGG','CCCCTA','TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getVerticalMatches(inputSample);
+      expect(result).to.equal(1);
+    });
+
+    it('hast to detect 1 diagonal match', function(){
+      var inputSample = ['ATGCGA','CAGTGC','TTATGT','AGAAGG','CCCCTA','TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getDiagonalMatches(inputSample);
+      expect(result).to.equal(1);
+    });
 });

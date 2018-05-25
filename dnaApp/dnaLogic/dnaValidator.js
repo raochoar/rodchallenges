02d9cd15353634bcd8f5a1da6dc8fe27ee60fa81
Rodrigo <rodrigo.ariel.ochoa@gmail.com>
2018-05-25 12:30:30 -0300

    Finish first version of dna Validator implementation

--- dnaApp/dnaLogic/dnaValidator.js
+++ dnaApp/dnaLogic/dnaValidator.js
@@ -2,6 +2,7 @@ var _ = require('lodash');
 
 function DnaValidator() {
   var validator = {};
+  var validLetters = ['A', 'C', 'G', 'T'];
 
   function hasHorizontalMatch(word) {
     var prevLetter = '';
@@ -16,21 +17,21 @@ function DnaValidator() {
         }
       } else {
         if (hasMatched) {
-         matches++;
+          matches++;
         }
         prevLetter = letter;
         count = 1;
         hasMatched = false;
       }
     });
-    if(hasMatched) { //last match case.
+    if (hasMatched) { //last match case.
       matches++;
     }
     return matches; //return true if it has a horizontal match.
   }
 
   validator.getHorizontalMatches = function (inputValues) {
-   var matcherCounter = 0;
+    var matcherCounter = 0;
     _.each(inputValues, function (word) {
       matcherCounter = matcherCounter + hasHorizontalMatch(word);
     });
@@ -41,25 +42,24 @@ function DnaValidator() {
   //to improve the process.
   function sanitizeInput(inputValues) {
     var result = [];
-    _.each(inputValues, function(row){
+    _.each(inputValues, function (row) {
       result.push(_.toArray(row)); // move into an array
     });
     return result;
   }
 
-  validator.getVerticalMatches = function(inputValues) {
-    inputValues = sanitizeInput(inputValues); //TODO: remove it
+  validator.getVerticalMatches = function (inputValues) {
+
     var newValues = _.zip.apply(_, inputValues); //transpond the matrix using lodash
     return validator.getHorizontalMatches(newValues);
   };
 
 
-  validator.getArrayOfDiagonalWords = function(inputValues) {
-    inputValues = sanitizeInput(inputValues); //TODO: remove it
+  validator.getArrayOfDiagonalWords = function (inputValues) {
+
     var newValues = [];
     //Work for NxN matrix where rows and cols are the same value. i> 2 because I need at least 4 items diag
-    for(var i=inputValues[0].length - 1; i > 2; i--)
-    {
+    for (var i = inputValues[0].length - 1; i > 2; i--) {
       var firstColDownUpDiag = [], lastColDownUpDiag = [];
       var firstColUpDownDiag = [], lastColUpDownDiag = [];
       var yDecrement = i;
@@ -72,8 +72,7 @@ function DnaValidator() {
       firstColUpDownDiag.push(inputValues[yIncrement][xIncrement]);
       lastColUpDownDiag.push(inputValues[yIncrement][xDecrement]);
 
-      while(yDecrement > 0 && xIncrement < inputValues[i].length && xDecrement > 0 && yIncrement < inputValues.length)
-      {
+      while (yDecrement > 0 && xIncrement < inputValues[i].length && xDecrement > 0 && yIncrement < inputValues.length) {
         yDecrement--;
         yIncrement++;
         xIncrement++;
@@ -86,7 +85,7 @@ function DnaValidator() {
 
       newValues.push(firstColDownUpDiag);
       newValues.push(lastColDownUpDiag);
-      if(inputValues.length - i - 1 > 0) { //Don't add main diagonal to avoid duplicated.
+      if (inputValues.length - i - 1 > 0) { //Don't add main diagonal to avoid duplicated.
         newValues.push(firstColUpDownDiag);
         newValues.push(lastColUpDownDiag);
       }
@@ -96,9 +95,79 @@ function DnaValidator() {
     return newValues;
   };
 
-  validator.getDiagonalMatches = function(inputValues) {
+  validator.getDiagonalMatches = function (inputValues) {
     var newValues = validator.getArrayOfDiagonalWords(inputValues);
     return validator.getHorizontalMatches(newValues);
+  };
+
+
+  validator.getValidMatrix = function (inputValues) {
+    var result = {isValid: true, message: ''};
+    if (!_.isArray(inputValues)) {
+      result.isValid = false;
+      result.message = 'Please enter a valid array of strings.';
+      return result;
+    }
+    var matrixSize = inputValues.length;
+    _.each(inputValues, function (row) {
+      if (!_.isString(row)) {
+        result.isValid = false;
+        result.message = 'The array has to be a valid array of just strings.';
+        return false;
+      }
+      if (row.length != matrixSize) {
+        result.isValid = false;
+        result.message = 'This API works only for NxN matrix. So height and width of the matrix has to be equal.';
+      }
+      var validCharacters = _.every(row, function (letter) {
+        return validLetters.indexOf(letter) >= 0;
+      });
+      if (!validCharacters) {
+        result.isValid = false;
+        result.message = 'Just valid dna letters are supported.';
+      }
+      return result.isValid; //Break the loop after the first error;
+    });
+    if (result.isValid) {
+      result.inputValues = sanitizeInput(inputValues); //convert into a valid matrix to improve usage of loadash.
+    }
+    return result;
+
+  };
+
+  /*
+    This method return isMutant true or false
+    if the input validation is not valid a null value will be returned
+   */
+  validator.isMutant = function (inputValues) {
+    var result = {
+      isMutant: null,
+      inputValidation: null
+    };
+
+    result.inputValidation = validator.getValidMatrix(inputValues);
+    if (result.inputValidation.isValid) {
+      var sanitizedMatrix = result.inputValidation.inputValues;
+      var matchesCounter = 0;
+      matchesCounter += validator.getHorizontalMatches(sanitizedMatrix);
+      if (matchesCounter > 1) {
+        result.isMutant = true;
+        return result;
+      }
+      matchesCounter += validator.getVerticalMatches(sanitizedMatrix);
+      if (matchesCounter > 1) {
+        result.isMutant = true;
+        return result;
+      }
+      matchesCounter += validator.getDiagonalMatches(sanitizedMatrix);
+      if (matchesCounter > 1) {
+        result.isMutant = true;
+        return result;
+      }
+      result.isMutant = false; //not a mutant
+    }
+    return result;
+
   };
 
   return validator;
--- dnaApp/test/dnaValidator.spec.js
+++ dnaApp/test/dnaValidator.spec.js
@@ -3,70 +3,178 @@ var expect = require('chai').expect;
 var dnaValidator = require('../dnaLogic/dnaValidator');
 
 describe('Validate dna values', function () {
-  it('Returns a  valid validator object', function () {
-    var validator = dnaValidator();
-    expect(validator).not.to.be.null;
+  describe('ADN patterns test', function () {
+    it('Returns a  valid validator object', function () {
+      var validator = dnaValidator();
+      expect(validator).not.to.be.null;
+    });
+
+    it('has to returns the number of horizontal matches', function () {
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getHorizontalMatches(inputSample);
+      expect(result).to.equal(1);
+    });
+
+    it('has to detect 2 matches in the same row', function () {
+      var inputSample = ['CCCCATTTTTCGGGACCCC'];
+      var validator = dnaValidator();
+      var result = validator.getHorizontalMatches(inputSample);
+      expect(result).to.equal(3);
+    });
+
+    it('has to detect 3 matches in different rows', function () {
+      var inputSample = ['AGGGGA', 'CAGTGC', 'TTATGT', 'AGAAAA', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getHorizontalMatches(inputSample);
+      expect(result).to.equal(3);
+    });
+
+    it('has to detect 2 matches with same letter', function () {
+      var inputSample = ['AAAAAATAAAA'];
+      var validator = dnaValidator();
+      var result = validator.getHorizontalMatches(inputSample);
+      expect(result).to.equal(2);
+    });
+
+    it('has to detect 1 vertical match', function () {
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      inputSample = validator.getValidMatrix(inputSample).inputValues;
+      var result = validator.getVerticalMatches(inputSample);
+      expect(result).to.equal(1);
+    });
+
+    it('hast to detect 1 diagonal match', function () {
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      inputSample = validator.getValidMatrix(inputSample).inputValues;
+      var result = validator.getDiagonalMatches(inputSample);
+      expect(result).to.equal(1);
+    });
+
+    it('has to return all the valid diagonals with at least 4 items', function () {
+      var expectedArray = [
+        ["T", "C", "A",  "T", "G", "A" ],
+        ["G", "T", "A",  "A", "A", "A" ],
+        ["C", "G", "A",  "T", "G"],
+        ["A", "G", "T",  "G", "T"],
+        ["C", "T", "A",  "C", "T"],
+        ["C", "G", "A",  "C", "C"],
+        ["A", "T", "G",  "C"],
+        ["G", "G", "T", "G"],
+        ["T", "G", "C", "C"],
+        ["T", "G", "C", "A"]
+      ];
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getArrayOfDiagonalWords(inputSample);
+      var arraysEquality = _.isEqual(result, expectedArray);
+      expect(arraysEquality).to.true;
+    });
   });
 
-  it('has to returns the number of horizontal matches', function () {
-    var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
-    var validator = dnaValidator();
-    var result = validator.getHorizontalMatches(inputSample);
-    expect(result).to.equal(1);
+  describe('Input validation unit tests', function(){
+    it('has to fail when the matrix size is corrupted', function(){
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTAAAATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getValidMatrix(inputSample);
+      expect(result.isValid).to.false;
+      expect(result.message).to.be.equal('This API works only for NxN matrix. So height and width of the matrix has to be equal.');
+    });
+
+    it('has to fail when the matrix character are invalid', function(){
+      var inputSample = ['ATGCGA', 'CAGTGC', 'AGAAGG', 'AGZAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getValidMatrix(inputSample);
+      expect(result.isValid).to.false;
+      expect(result.message).to.be.equal('Just valid dna letters are supported.');
+    });
+
+    it('has to fail when the matrix is not a array of strings', function(){
+      var inputSample = ['ATGCGA', 'CAGTGC', 2, 'AGZAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.getValidMatrix(inputSample);
+      expect(result.isValid).to.false;
+      expect(result.message).to.be.equal('The array has to be a valid array of just strings.');
+    });
+
+    it('has to fail when the input is not a array character are invalid', function(){
+      var inputSample = 'ATGCA';
+      var validator = dnaValidator();
+      var result = validator.getValidMatrix(inputSample);
+      expect(result.isValid).to.false;
+      expect(result.message).to.be.equal('Please enter a valid array of strings.');
+    });
+
+
+
+    it('has to return true if the matrix is valid', function(){
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
+      var expectedMatrix = [
+        [ "A", "T", "G", "C", "G", "A"],
+        [ "C", "A", "G", "T", "G", "C"],
+        [ "T", "T", "A", "T", "G", "T"],
+        [ "A", "G", "A", "A", "G", "G"],
+        [ "C", "C", "C", "C", "T", "A"],
+        [ "T", "C", "A", "C", "T", "G"]
+      ];
+      var validator = dnaValidator();
+      var result = validator.getValidMatrix(inputSample);
+      expect(result.isValid).to.true;
+      expect(_.isEqual(result.inputValues, expectedMatrix)).to.true;
+      expect(result.message).to.be.equal('');
+    });
   });
 
-  it('has to detect 2 matches in the same row', function () {
-    var inputSample = ['CCCCATTTTTCGGGACCCC'];
-    var validator = dnaValidator();
-    var result = validator.getHorizontalMatches(inputSample);
-    expect(result).to.equal(3);
-  });
+  describe('Test of isMutant main method', function() {
+    it('has to detect mutant dna', function(){
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.isMutant(inputSample);
+      expect(result.isMutant).to.be.true;
+    });
 
-  it('has to detect 3 matches in different rows', function () {
-    var inputSample = ['AGGGGA', 'CAGTGC', 'TTATGT', 'AGAAAA', 'CCCCTA', 'TCACTG'];
-    var validator = dnaValidator();
-    var result = validator.getHorizontalMatches(inputSample);
-    expect(result).to.equal(3);
-  });
+    it('has to detect mutant dna with 2 diagonal matches', function(){
+      var inputSample = [
+        'ACAACA',
+        'CAGTGA',
+        'TTATAT',
+        'AGAAGG',
+        'CTACTA',
+        'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.isMutant(inputSample);
+      expect(result.isMutant).to.be.true;
+    });
 
-  it('has to detect 2 matches with same letter', function () {
-    var inputSample = ['AAAAAATAAAA'];
-    var validator = dnaValidator();
-    var result = validator.getHorizontalMatches(inputSample);
-    expect(result).to.equal(2);
-  });
+    it('has to detect mutant dna with 2 horizontal matches', function(){
+      var inputSample = ['AAAACA',
+                         'CAGTGC',
+                         'TTATGT',
+                         'AGAAGG',
+                         'CCCCTA',
+                         'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.isMutant(inputSample);
+      expect(result.isMutant).to.be.true;
+    });
 
-  it('has to detect 1 vertical match', function () {
-    var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
-    var validator = dnaValidator();
-    var result = validator.getVerticalMatches(inputSample);
-    expect(result).to.equal(1);
-  });
+    it('has to detect regular dna non-mutant', function() {
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATTT', 'AGACGG', 'GCGTCA', 'TCACTG'];
+      var validator = dnaValidator();
+      var result = validator.isMutant(inputSample);
+      expect(result.isMutant).to.be.false;
+    });
 
-  it('hast to detect 1 diagonal match', function () {
-    var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
-    var validator = dnaValidator();
-    var result = validator.getDiagonalMatches(inputSample);
-    expect(result).to.equal(1);
-  });
+    it('has to detect invalid inputs', function() {
+      var inputSample = ['ATGCGA', 'CAGTGC', 'TTATTT', 'AGACGG', 'GCGTCA', 'TCAAAAACTG'];
+      var validator = dnaValidator();
+      var result = validator.isMutant(inputSample);
+      expect(result.isMutant).to.be.null;
+      expect(result.inputValidation.isValid).to.be.false;
+    });
+    //TODO: Add more scenarios here.
 
-  it('has to return all the valid diagonals with at least 4 items', function () {
-    var expectedArray = [
-      ["T", "C", "A",  "T", "G", "A" ],
-      ["G", "T", "A",  "A", "A", "A" ],
-      ["C", "G", "A",  "T", "G"],
-      ["A", "G", "T",  "G", "T"],
-      ["C", "T", "A",  "C", "T"],
-      ["C", "G", "A",  "C", "C"],
-      ["A", "T", "G",  "C"],
-      ["G", "G", "T", "G"],
-      ["T", "G", "C", "C"],
-      ["T", "G", "C", "A"]
-    ];
-    var inputSample = ['ATGCGA', 'CAGTGC', 'TTATGT', 'AGAAGG', 'CCCCTA', 'TCACTG'];
-    var validator = dnaValidator();
-    var result = validator.getArrayOfDiagonalWords(inputSample);
-    var arraysEquality = _.isEqual(result, expectedArray);
-    expect(arraysEquality).to.true;
   });
 });
